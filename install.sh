977a3d533c93ba33aac2a4b2579fcfcef4dd35b6
ivsaav <ivo_saavedra@outlook.com>
2021-12-11 14:45:22 +0000

    Add: install script and simple_sw qrels file

---
+++ install.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+./root_scripts/load_schemas.sh
+./root_scripts/index.sh
--- py_scripts/eval.py
+++ py_scripts/eval.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import requests
+import io
 
 from pathlib import Path
 from sklearn.metrics import PrecisionRecallDisplay
@@ -18,7 +19,7 @@ from sklearn.metrics import PrecisionRecallDisplay
 # { OPTIONS SYSTEM 2
 #     'q.OP' : 'AND',
 #     'defType': 'edismax',
-#     'qf': title^1.2 genre kind^0.8 language cast writer composer plot^1.5,
+#     'qf': title^1.2 genre^1.1 kind^0.8 language cast writer composer plot^0.7,
 #     'rows': 100,
 #     'fl': 'id'
 # }
@@ -26,24 +27,34 @@ from sklearn.metrics import PrecisionRecallDisplay
 
 #### WW2 no documentaries ####
 # QNAME = "ww2_no_docs"
-# QUERY_URL1 = "http://localhost:8983/solr/netflix/select?defType=edismax&fl=id%20title%20plot&indent=true&q.op=AND&q=%22World%20War%22%20(2%20OR%20II%20OR%20two)%20(action%20OR%20drama%20OR%20thriller)%20AND%20-documentary&qf=title%20genre%20kind%20language%20cast%20writer%20composer%20plot&rows=100"
-# QUERY_URL2 = "http://localhost:8983/solr/netflix/select?defType=edismax&fl=id%20genre%20plot&indent=true&q.op=AND&q=%22World%20War%22%20(2%20OR%20II%20OR%20two)%20(action%20OR%20drama%20OR%20thriller)%20AND%20-documentary&qf=title%5E1.2%20genre%20kind%5E0.8%20language%20cast%20writer%20composer%20plot%5E1.5&rows=100"
+# QUERY_URL1 = "http://localhost:8983/solr/netflix/select?defType=edismax&indent=true&q.op=AND&q=%22World%20War%22%20(2%20OR%20II%20OR%20two)%20(action%20OR%20drama%20OR%20thriller)%20AND%20-documentary&qf=title%20genre%20kind%20language%20cast%20writer%20composer%20plot&rows=100"
+# # QUERY_URL2 = "http://localhost:8983/solr/netflix/select?defType=edismax&fl=id%20genre%20plot&indent=true&q.op=AND&q=%22World%20War%22%20(2%20OR%20II%20OR%20two)%20(action%20OR%20drama%20OR%20thriller)%20AND%20-documentary&qf=title%5E1.2%20genre%20kind%5E0.8%20language%20cast%20writer%20composer%20plot%5E1.5&rows=100"
+# QUERY_URL2 = "http://localhost:8983/solr/netflix/select?defType=edismax&indent=true&q.op=AND&q=%22World%20War%22%20(2%20OR%20II%20OR%20two)%20(action%20OR%20drama%20OR%20thriller)%20AND%20-documentary&qf=title%5E1.2%20genre%5E1.1%20kind%5E0.8%20language%20cast%20writer%20composer%20plot%5E0.7&rows=100"
 
 #### family programs done by voice actors ####
 # QNAME = "voice_actors_family"
 # QUERY_URL1 = "http://localhost:8983/solr/netflix/select?defType=edismax&fl=id%20title%20plot&indent=true&q.op=AND&q=(%22Frank%20Welker%22%20OR%20%22Kirk%20Thornton%22%20OR%20%22Wendee%20Lee%22%20OR%20%22Jeff%20Bennett%22)%20AND%20Family&qf=title%20genre%20kind%20language%20cast%20writer%20composer%20plot&rows=100"
-# QUERY_URL2 = "http://localhost:8983/solr/netflix/select?defType=edismax&fl=id%20genre%20plot&indent=true&q.op=AND&q=(%22Frank%20Welker%22%20OR%20%22Kirk%20Thornton%22%20OR%20%22Wendee%20Lee%22%20OR%20%22Jeff%20Bennett%22)%20AND%20Family&qf=title%5E1.2%20genre%20kind%5E0.8%20language%20cast%20writer%20composer%20plot%5E1.5&rows=100"
+# # QUERY_URL2 = "http://localhost:8983/solr/netflix/select?defType=edismax&fl=id%20genre%20plot&indent=true&q.op=AND&q=(%22Frank%20Welker%22%20OR%20%22Kirk%20Thornton%22%20OR%20%22Wendee%20Lee%22%20OR%20%22Jeff%20Bennett%22)%20AND%20Family&qf=title%5E1.2%20genre%20kind%5E0.8%20language%20cast%20writer%20composer%20plot%5E1.5&rows=100"
+# QUERY_URL2 = "http://localhost:8983/solr/netflix/select?defType=edismax&indent=true&q.op=AND&q=(%22Frank%20Welker%22%20OR%20%22Kirk%20Thornton%22%20OR%20%22Wendee%20Lee%22%20OR%20%22Jeff%20Bennett%22)%20AND%20Family&qf=title%5E1.2%20genre%5E1.1%20kind%5E0.8%20language%20cast%20writer%20composer%20plot%5E0.7&rows=100"
 
 ##### romantic comedies in spanish or french #####
 # QNAME = "comedy_romantic_fr_spa"
 # QUERY_URL1 = "http://localhost:8983/solr/netflix/select?defType=edismax&fl=id%20title%20plot&indent=true&q.op=AND&q=(spanish%20OR%20french)%20AND%20(comedy%20AND%20romance)&qf=title%20genre%20kind%20language%20cast%20writer%20composer%20plot&rows=100"
-# QUERY_URL2 = "http://localhost:8983/solr/netflix/select?defType=edismax&fl=id%20genre%20plot&indent=true&q.op=AND&q=(spanish%20OR%20french)%20AND%20(comedy%20AND%20romance)&qf=title%5E1.2%20genre%20kind%5E0.8%20language%20cast%20writer%20composer%20plot%5E1.5&rows=100"
+# # QUERY_URL2 = "http://localhost:8983/solr/netflix/select?defType=edismax&fl=id%20genre%20plot&indent=true&q.op=AND&q=(spanish%20OR%20french)%20AND%20(comedy%20AND%20romance)&qf=title%5E1.2%20genre%20kind%5E0.8%20language%20cast%20writer%20composer%20plot%5E1.5&rows=100"
+# QUERY_URL2 = "http://localhost:8983/solr/netflix/select?defType=edismax&indent=true&q.op=AND&q=(spanish%20OR%20french)%20AND%20(comedy%20AND%20romance)&qf=title%5E1.2%20genre%5E1.1%20kind%5E0.8%20language%20cast%20writer%20composer%20plot%5E0.7&rows=100"
 
 ##### comedy series in english #####
-QNAME = "series_comedy_english_to1995"
-QUERY_URL1 = "http://localhost:8983/solr/netflix/select?defType=edismax&fl=id%20title%20plot&fq=year%3A%5B*%20TO%201995%5D&indent=true&q.op=AND&q=(%22tv%20series%22%20OR%20%22tv%20mini%20series%22%20OR%20%22series%22)%20AND%20Comedy%20AND%20English&qf=title%20genre%20kind%20language%20cast%20writer%20composer%20plot&rows=100"
-QUERY_URL2 = "http://localhost:8983/solr/netflix/select?defType=edismax&fl=id%20genre%20plot&fq=year%3A%5B*%20TO%201995%5D&indent=true&q.op=AND&q=(%22tv%20series%22%20OR%20%22tv%20mini%20series%22%20OR%20%22series%22)%20AND%20Comedy%20AND%20English&qf=title%5E1.2%20genre%20kind%5E0.8%20language%20cast%20writer%20composer%20plot%5E1.5&rows=100"
+# QNAME = "series_comedy_english_to1995"
+# QUERY_URL1 = "http://localhost:8983/solr/netflix/select?defType=edismax&fl=id%20title%20plot&fq=year%3A%5B*%20TO%201995%5D&indent=true&q.op=AND&q=(%22tv%20series%22%20OR%20%22tv%20mini%20series%22%20OR%20%22series%22)%20AND%20Comedy%20AND%20English&qf=title%20genre%20kind%20language%20cast%20writer%20composer%20plot&rows=100"
+# # QUERY_URL2 = "http://localhost:8983/solr/netflix/select?defType=edismax&fl=id%20genre%20plot&fq=year%3A%5B*%20TO%201995%5D&indent=true&q.op=AND&q=(%22tv%20series%22%20OR%20%22tv%20mini%20series%22%20OR%20%22series%22)%20AND%20Comedy%20AND%20English&qf=title%5E1.2%20genre%20kind%5E0.8%20language%20cast%20writer%20composer%20plot%5E1.5&rows=100"
+# QUERY_URL2 = "http://localhost:8983/solr/netflix/select?defType=edismax&fq=year%3A%5B*%20TO%201995%5D&indent=true&q.op=AND&q=(%22tv%20series%22%20OR%20%22tv%20mini%20series%22%20OR%20%22series%22)%20AND%20Comedy%20AND%20English&qf=title%5E1.2%20genre%5E1.1%20kind%5E0.8%20language%20cast%20writer%20composer%20plot%5E0.7&rows=100"
 
+
+#### star wars ambiguity search ####
+QNAME = "simple_sw"
+QUERY_URL1 = "http://localhost:8983/solr/netflix/select?defType=edismax&indent=true&q.op=AND&q=Star%20Wars&qf=title%20genre%20kind%20language%20cast%20writer%20composer%20plot&rows=100"
+# QUERY_URL2 = "http://localhost:8983/solr/netflix/select?defType=edismax&fl=id%20title%20genre%20plot%20composer&indent=true&q.op=AND&q=Star%20Wars&qf=title%5E1.2%20genre%20kind%5E0.8%20language%20cast%20writer%20composer%20plot%5E1.5&rows=50"
+QUERY_URL2 ="http://localhost:8983/solr/netflix/select?defType=edismax&indent=true&q.op=AND&q=Star%20Wars&qf=title%5E1.2%20genre%5E1.1%20kind%5E0.8%20language%20cast%20writer%20composer%20plot%5E0.7&rows=100"
 
 res_sys1 = requests.get(QUERY_URL1).json()['response']['docs']
 res_sys2 = requests.get(QUERY_URL2).json()['response']['docs']
@@ -80,38 +91,89 @@ def p10(results, relevant, n=10):
     """Precision at N"""
     return len([doc for doc in results[:n] if doc['id'] in relevant])/n
 
+def convertToLaTeX(df, alignment="|c"):
+    """
+    Convert a pandas dataframe to a LaTeX tabular.
+    Prints labels in bold, does not use math mode
+    """
+    numColumns = df.shape[1]
+    numRows = df.shape[0]
+    output = io.StringIO()
+    colFormat = ("%s%s|" % (alignment, alignment * numColumns))
+    
+    #Write header
+    output.write("\\begin{table}[]\n")
+    output.write("\\begin{tabular}{%s} \\hline\n" % colFormat)
+    columnLabels = ["\\textbf{%s}" % label for label in df.columns]
+    output.write("& %s\\\\\\hline\n" % " & ".join(columnLabels))
+    #Write data lines
+    for i in range(numRows):
+        output.write("\\textbf{%s} & %s\\\\ \\hline\n"
+                     % (df.index[i], " & ".join([str(val) for val in df.iloc[i]])))
+    #Write footer
+    output.write("\\end{tabular}\n\\end{table}")
+    return output.getvalue()
+
+def ranking(res1, res2, relevant, n=10):
+    # df = pd.DataFrame([['Rank','SYS1', 'SYS2']] +
+    #     [
+    #         [[i for i in range(n+1)], ["X" if doc['id'] in relevant else " " for doc in res1[:n] ],  ["X" if doc['id'] in relevant else " " for doc in res2[:n]]]
+    #     ]
+    # )
+    
+    df = pd.DataFrame(
+        {
+            'Rank' : [i for i in range(1, n+1)],
+            'SYS1' : ["X" if doc['id'] in relevant else "" for doc in res1[:n] ],
+            'SYS2':  ["X" if doc['id'] in relevant else "" for doc in res2[:n] ]
+        }
+    )
+    df = df.set_index('Rank')
+    with open(Path(f'../docs/eval_reports/{QNAME}_rank.tex'),'w') as tf:
+        # tf.write(df.to_latex(index=False))
+        tf.write(convertToLaTeX(df))
+    print(df)
+
 def calculate_metric(key, results, relevant):
     return metrics[key](results, relevant)
 
 # Define metrics to be calculated
 evaluation_metrics = {
-    'ap': 'Average Precision',
-    'p10': 'Precision at 10 (P@10)'
+    'ap': 'AP',
+    'p10': 'P@10'
 }
 
 # Calculate all metrics and export results as LaTeX table
-df = pd.DataFrame([['Metric','SYS1', 'SYS2']] +
-    [
-        [evaluation_metrics[m], calculate_metric(m, res_sys1, relevant),  calculate_metric(m, res_sys2, relevant)]
-        for m in evaluation_metrics
-    ]
-)
+# df = pd.DataFrame([['Metric','SYS1', 'SYS2']] +
+#     [
+#         [evaluation_metrics[m], calculate_metric(m, res_sys1, relevant),  calculate_metric(m, res_sys2, relevant)]
+#         for m in evaluation_metrics
+#     ]
+# )
+
+df = pd.DataFrame(
+        {
+            "Metric": [evaluation_metrics[m] for m in evaluation_metrics],
+            "SYS1" :  [calculate_metric(m, res_sys1, relevant) for m in evaluation_metrics],
+            "SYS2" :  [calculate_metric(m, res_sys2, relevant) for m in evaluation_metrics]
+        }
+    )
+df.set_index('Metric', inplace=True)
 
 with open(Path(f'../docs/eval_reports/{QNAME}_results.tex'),'w') as tf:
-    tf.write(df.to_latex())
+    tf.write(convertToLaTeX(df))
 
 #################################################################################
 #################################################################################
 # PRECISION-RECALL CURVE
 # Calculate precision and recall values as we move down the ranked list
 
-def plot_precision_recal_graph(results, relevant, **kwargs): 
+def plot_precision_recal_graph(results, relevant, avp=None, **kwargs): 
     
     # results = results[:20]
     precision_values = [
         len([
-            doc 
-            for doc in results[:idx]
+            doc for doc in results[:idx]
             if doc['id'] in relevant
         ]) / idx 
         for idx, _ in enumerate(results, start=1)
@@ -125,27 +187,22 @@ def plot_precision_recal_graph(results, relevant, **kwargs):
         for idx, _ in enumerate(results, start=1)
     ]
 
-    precision_recall_match = {k: v for k,v in zip(recall_values, precision_values)}
-
-    # Extend recall_values to include traditional steps for a better curve (0.1, 0.2 ...)
-    recall_values.extend([step for step in np.arange(0.1, 1.1, 0.1) if step not in recall_values])
-    recall_values = sorted(set(recall_values))
-
-    # Extend matching dict to include these new intermediate steps
-    for idx, step in enumerate(recall_values):
-        if step not in precision_recall_match:
-            if recall_values[idx-1] in precision_recall_match:
-                precision_recall_match[step] = precision_recall_match[recall_values[idx-1]]
-            else:
-                precision_recall_match[step] = precision_recall_match[recall_values[idx+1]]
-
-    disp = PrecisionRecallDisplay([precision_recall_match.get(r) for r in recall_values], recall_values)
+    # print("precision")
+    # print(precision_values)
+    
+    # print("recal")
+    # print(recall_values)
+    # disp = PrecisionRecallDisplay([precision_recall_match.get(r) for r in recall_values], recall_values)
+    disp = PrecisionRecallDisplay(precision=precision_values, recall=recall_values, average_precision=avp)
     disp.plot(**kwargs)
     # plt.savefig('precision_recall.pdf')
 
-_, ax = plt.subplots(figsize=(7, 8))
+_, ax = plt.subplots(figsize=(8, 8))
   
-plot_precision_recal_graph(res_sys1, relevant, name="sys1", color="red", ax=ax)
-plot_precision_recal_graph(res_sys2, relevant, name="sys2", color="orange", ax=ax)
+plot_precision_recal_graph(res_sys1, relevant,  name="sys1", color="red", ax=ax)
+plot_precision_recal_graph(res_sys2, relevant, name="sys2",  color="orange", ax=ax)
 
-plt.show()
+ax.set_ylim([0.3, 1.01])
+plt.savefig(Path(f'../docs/eval_reports/{QNAME}_curve.png'), bbox_inches='tight')
+
+ranking(res_sys1, res_sys2, relevant, 10)
